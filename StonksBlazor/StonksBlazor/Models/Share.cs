8a2e3414fba58653f94a5c3b4862bd44238e6018
NilsNiBe <nils.becker@rwth-aachen.de>
2022-04-18 07:45:37 +0200

    initial

---
+++ StonksBlazor/StonksBlazor/Models/Share.cs
@@ -0,0 +1,6 @@
+﻿namespace StonksBlazor.Models
+{
+  public record Share(string Symbol, List<Purchase> Purchases, object ChartResult);
+
+  public record Purchase(string Id, double Timestamp, int Amount);
+}
